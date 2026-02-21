a2333885d08fc5fd01a8b6c37a122588d2eb07cf
navid <nkamareh@gmail.com>
2026-02-21 09:18:54 +0100

    corrected the PR comments

--- only_odds.py
+++ only_odds.py
@@ -1,7 +1,6 @@
 #issue 1
-total = 0
+
 for i in range(1, 11):
     if i % 2 == 1:  
-        total += i
-print("The sum of odd numbers from 1 to 10 is:", total) 
+        print("Odd numbers from 1 to 10 are:", i) 
 
