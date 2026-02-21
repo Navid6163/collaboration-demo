216f184787b5c2a7b9f39b06a580195e439bd85d
Navid6163 <98782944+Navid6163@users.noreply.github.com>
2026-02-21 09:41:13 +0100

    add more comment and fix: #2 (#4)
    * add more comment and add a comment at the end. fix: #2

--- only_odds.py
+++ only_odds.py
@@ -1,6 +1,9 @@
-#issue 1
+#issue 1 resolved
+#issue 2 resolved
 
+# this proram prints odd numbers from 1 to 10
 for i in range(1, 11):
-    if i % 2 == 1:  
+    if i % 2 == 1:  # check if the number is odd
         print("Odd numbers from 1 to 10 are:", i) 
 
+# end of the program
